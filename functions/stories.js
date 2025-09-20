480ac675bb2cb9705c5a58ca9ff4507c7c0a3006
Jasper Riedel <jasper@riedel.tech>
2025-09-20 13:12:34 +1000

    Added stories API worker functions

---
+++ functions/stories.js
@@ -0,0 +1,52 @@
+export async function onRequestGet(context) {
+  const query = context.env.db.prepare("SELECT * FROM stories")
+  const results = await query.raw()
+  return Response.json(results)
+}
+
+export async function onRequestPost(context) {
+  const body = await context.request.json()
+  const timestamp = Date.now()
+
+  if (body.code == context.env.INSTALLATION_KEY) {
+    try {
+      const result = await context.env.db
+        .prepare("INSERT INTO stories (timestamp, content) VALUES (?, ?)")
+        .bind(timestamp, body.content)
+        .run()
+
+      if (result.success) {
+        return new Response.json(
+          {
+            message: "Story received successfully.",
+            timestamp,
+          }
+        )
+      } else {
+        return new Response.json(
+          {
+            message: "Failed to record story on backend.",
+          }
+        )
+      }
+    } catch (error) {
+      return new Response.json(
+        {
+          message: "There was a database error."
+        },
+        {
+          status: 500
+        }
+      )
+    }
+  } else {
+    return new Response.json(
+      {
+        message: "The code " + body.code + " has expired or was invalid."
+      },
+      {
+        status: 400
+      }
+    )
+  }
+}

1e078ab7a78efc5f03f58e824c0befda098ba0e6
Jasper Riedel <jasper@riedel.tech>
2025-09-20 17:26:40 +1000

    Clean up PIN code handling on the installation

--- installation/src/main.ts
+++ installation/src/main.ts
@@ -1,37 +1,54 @@
 import { BASE_URL } from "../../shared/shared"
 import './style.css'
 
+/**
+ * The key used to authenticate the installation with the backend service.
+ */
 const INSTALLATION_KEY = "c757ae22-2be3-461c-9ab4-53da3ea87a29"
 
-// Handle updating and displaying PIN code
-const generatePin = (): string => {
+/**
+ * Generate a new random 4 digit code.
+ *
+ * @returns 4 digit code
+ */
+const generateCode = (): string => {
   const pin = 1000 + Math.floor(Math.random() * 8999)
   return pin.toString()
 }
-let currentPin = ""
-const updatePin = () => {
+
+/**
+ * Contains the current PIN code that the installation will accept submissions from.
+ */
+let code = ""
+
+/**
+ * Generate a new PIN code and update on the backend service.
+ */
+const updateCode = () => {
   const linkPinElement = document.getElementById("link-pin")
-  currentPin = generatePin()
+  code = generateCode()
   fetch(BASE_URL + "/codes", {
     method: "PATCH",
     headers: {
       "X-INSTALLATION-KEY": INSTALLATION_KEY
     },
-    body: JSON.stringify({ code: currentPin })
+    body: JSON.stringify({ code: code })
   })
     .then(res => {
       if (res.status == 200 && linkPinElement) {
-        linkPinElement.innerText = currentPin
+        linkPinElement.innerText = code
       } else {
-        console.log("Failed to update PIN code on backend.")
+        console.error("Failed to update PIN code on backend.")
       }
     })
     .catch(error => {
       console.error("PIN Code update request failed: " + error)
     })
 }
-setInterval(() => updatePin(), 5 * 60 * 1000)
-updatePin()
+
+// Setup and initial PIN code and then regenerate it every 5 minutes. 
+updateCode()
+setInterval(() => updateCode(), 5 * 60 * 1000)
 
 // Handle story content on beams
 // interface Story {

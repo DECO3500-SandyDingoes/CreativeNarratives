52981395234e47a0140bc7f537e0ed51708dbd93
Jasper Riedel <jasper@riedel.tech>
2025-10-14 15:45:48 +1000

    Swap editor page for start page as index

--- editor/src/start.ts
+++ editor/src/start.ts
@@ -2,4 +2,5 @@ document.getElementById("connect-button")
   ?.addEventListener("click", () => {
     const code = (document.getElementById("connect-code")! as HTMLInputElement).value
     // TODO: Request content ID from API here and redirect to editor
+    window.location.assign("editor.html")
   })

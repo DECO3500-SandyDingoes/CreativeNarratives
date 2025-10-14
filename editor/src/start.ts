ba273f4239c9235f232a99d410b40be74b99ce75
Jasper Riedel <jasper@riedel.tech>
2025-10-14 16:28:34 +1000

    Resolve unused code warning to allow for serverless build

--- editor/src/start.ts
+++ editor/src/start.ts
@@ -1,6 +1,6 @@
 document.getElementById("connect-button")
   ?.addEventListener("click", () => {
-    const code = (document.getElementById("connect-code")! as HTMLInputElement).value
+    // const code = (document.getElementById("connect-code")! as HTMLInputElement).value
     // TODO: Request content ID from API here and redirect to editor
     window.location.assign("editor.html")
   })

698e0e11d43cd8736b922acbc8a5b55a5c222550
Jasper Riedel <jasper@riedel.tech>
2025-10-14 15:42:15 +1000

    Added basic code entry screen

---
+++ editor/src/start.ts
@@ -0,0 +1,5 @@
+document.getElementById("connect-button")
+  ?.addEventListener("click", () => {
+    const code = (document.getElementById("connect-code")! as HTMLInputElement).value
+    // TODO: Request content ID from API here and redirect to editor
+  })
